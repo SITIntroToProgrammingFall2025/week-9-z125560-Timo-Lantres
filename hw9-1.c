03a69b505798e106c5e753149088f471a12c6e84
z125560-Timo-Lantres <z125560@shibaura-it.ac.jp>
2025-12-04 13:27:35 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,44 @@
+/*
+Lantrès Timo
+Student ID: z125560
+Lesson Number: 9
+Date: 03/12/2025
+*/
+
+# include <stdio.h>
+
+int main(void)
+{
+    int a[2][2] ;
+    int i, j ;
+    double det ;
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for (j = 0 ; j < 2 ; j++)
+        {
+            //printf("Enter the element in [%d][%d]: ", i, j) ;
+            scanf("%d", &a[i][j]) ;
+        }
+    }
+
+    printf("You entered\n") ;
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for(j = 0 ; j < 2 ; j++)
+        {
+            printf("%d ", a[i][j]) ;
+        }
+        printf("\n") ;
+    }
+
+    det = a[0][0] * a[1][1] - a[1][0] * a[0][1] ;
+
+    printf("Determinant is %f", det) ;
+
+    return 0 ;
+
+}
+
 
