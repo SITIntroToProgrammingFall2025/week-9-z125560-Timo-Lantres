13d84c5d77348782e04e4e22de37c7114275ba87
z125560-Timo-Lantres <z125560@shibaura-it.ac.jp>
2025-12-04 13:27:58 +0900

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,81 @@
+/*
+Lantrès Timo
+Student ID: z125560
+Lesson Number: 9
+Date: 03/12/2025
+*/
+
+# include <stdio.h>
+
+int main(void)
+{
+    int a[2][3], b[3][2], c[2][2] ;
+    int i, j, k ;
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for (j = 0 ; j < 3 ; j++)
+        {
+            //printf("Enter the element in [%d][%d]: ", i, j) ;
+            scanf("%d", &a[i][j]) ;
+        }
+    }
+
+    for(i = 0 ; i < 3 ; i++)
+    {
+        for (j = 0 ; j < 2 ; j++)
+        {
+            //printf("Enter the element in [%d][%d]: ", i, j) ;
+            scanf("%d", &b[i][j]) ;
+        }
+    }
+
+    printf("The first matrix you entered is\n") ;
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for(j = 0 ; j < 3 ; j++)
+        {
+            printf("%d ", a[i][j]) ;
+        }
+        printf("\n") ;
+    }
+
+    printf("The second matrix you entered is\n") ;
+
+    for(i = 0 ; i < 3 ; i++)
+    {
+        for(j = 0 ; j < 2 ; j++)
+        {
+            printf("%d ", b[i][j]) ;
+        }
+        printf("\n") ;
+    }
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for(j = 0 ; j < 2 ; j++)
+        {
+            c[i][j] = 0 ;
+            for(k = 0 ; k < 3 ; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j] ;
+            }
+        }
+    }
+
+    printf("The multiplication product of matrix A and matrix B :\n") ;
+
+    for(i = 0 ; i < 2 ; i++)
+    {
+        for(j = 0 ; j < 2 ; j++)
+        {
+            printf("%d ", c[i][j]) ;
+        }
+        printf("\n") ;
+    }
+
+    return 0 ;
+}
+
 
