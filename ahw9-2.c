f49486b4415ef2385401caf5433d3485bada7049
z125560-Timo-Lantres <z125560@shibaura-it.ac.jp>
2025-12-04 13:27:23 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,43 @@
+/*
+Lantrès Timo
+Student ID: z125560
+Lesson Number: 9
+Date: 03/12/2025
+*/
+
+# include <stdio.h>
+# include <math.h>
+
+int main(void)
+{
+    int i, j, temp ;
+
+    int a[5] ;
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        scanf("%d", &a[i]) ;
+    }
+
+    for (i = 0 ; i < 4 ; i++)
+    {
+        for (j = 0 ; j < 4 ; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j] ;
+                a[j] = a[j + 1] ;
+                a[j + 1] = temp ;
+            }
+        }
+    }
+
+    for (i = 0 ; i < 5 ; i++)
+    {
+        printf("%d ", a[i]) ;
+    }
+
+    return 0 ;
+}
+
 
